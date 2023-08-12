3b7a5ae755c42452016c210f47d66866a7c26c50
Cristian Martinez <107958139+cmartinezcxm@users.noreply.github.com>
2023-08-11 23:59:32 -0500

    Add Converter

--- ExampleApp/ExampleApp/ExampleApp/Views/ClientPage.xaml.cs
+++ ExampleApp/ExampleApp/ExampleApp/Views/ClientPage.xaml.cs
@@ -20,7 +20,10 @@ namespace ExampleApp.Views
 
         private void _viewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == nameof(ClientViewModel.Client))
+            if (e.PropertyName == nameof(ClientViewModel.Client) &&
+                _viewModel.Client.Latitude != default &&
+                _viewModel.Client.Longitude != default &&
+                string.IsNullOrEmpty(_viewModel.Client.Name))
             {
                 var position = new Position(_viewModel.Client.Latitude, _viewModel.Client.Longitude);
                 ClientLocationMap.MoveToRegion(new MapSpan(position, 0.01, 0.01));
