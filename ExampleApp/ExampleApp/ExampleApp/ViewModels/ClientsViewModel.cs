3e41ead399dc1e0a97336588c4a893c23c220344
Cristian Martinez <107958139+cmartinezcxm@users.noreply.github.com>
2023-08-22 17:07:51 -0500

    Use RefreshView

--- ExampleApp/ExampleApp/ExampleApp/ViewModels/ClientsViewModel.cs
+++ ExampleApp/ExampleApp/ExampleApp/ViewModels/ClientsViewModel.cs
@@ -18,19 +18,24 @@ namespace ExampleApp.ViewModels
         {
             AppearingCommand = new AsyncCommand(async () => await OnAppearingAsync());
             ClientTappedCommand = new AsyncCommand<Client>(OnClientTapped);
+            RefreshCommand = new AsyncCommand(OnRefresh);
             Title = "Clients";
             _clientService = clientService;
         }
 
+        private bool _isRefreshing = false;
+
+        public bool IsRefreshing { get => _isRefreshing; set => SetProperty(ref _isRefreshing, value); }
         public ObservableRangeCollection<Client> Clients { get; set; } = new ObservableRangeCollection<Client>();
 
 
         public ICommand AppearingCommand { get; set; }
         public ICommand ClientTappedCommand { get; set; }
+        public ICommand RefreshCommand { get; set; }
 
         private async Task OnAppearingAsync()
         {
-            await LoadData();
+            IsRefreshing = true;
         }
 
         private async Task LoadData()
@@ -63,5 +68,12 @@ namespace ExampleApp.ViewModels
 
             return Shell.Current.GoToAsync($"{nameof(ClientPage)}?{nameof(ClientViewModel.ClientId)}={client.Id}");
         }
+
+        private async Task OnRefresh()
+        {
+            await LoadData();
+            IsRefreshing = false;
+        }
+
     }
 }
